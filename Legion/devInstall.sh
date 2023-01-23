bc37ae84f1c85bb9c963aa6bb7e56bd961047d8c
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2023-01-23 14:22:05 -0800

    updated install scripts

--- Legion/devInstall.sh
+++ Legion/devInstall.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/sh
 
-echo "\nInstalling dev tools\n"
-pip install pillow # ranger image preview
+echo
+echo "Installing dev tools: TypeScript, Sass, & live-server, pillow"
+echo
+
+sudo -H pip install pillow # ranger image preview
 npm install -g typescript sass live-server # web dev
 
-# term candy
+# Version check
+echo "Installed:"
+echo
+echo "pillow $(pip show pillow | grep "Version")"
+echo "TypeScript $(tsc -v)"
+echo "Sass v$(sass --version)"
+live-server -v
+
+# terminal candy
 paru -S --needed pfetch cmatrix-git ttf-sourcecodepro-nerd
--- Legion/install.sh
+++ Legion/install.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/sh
 
-echo "\n!WARNING! This will overwrite configs for: kitty, neovim, & ranger...!\n"
+echo
+echo "!WARNING! This will overwrite configs for: kitty, neovim, & ranger...!"
+echo
 sleep 2
 
 # Base install
 sudo pacman -S --needed neovim kitty ranger python-pip neofetch nodejs npm exa bat bottom
 
-# Config installs
+# Version check
+echo "Installed:"
+echo
+nvim -v | grep "NVIM"
+kitty -v
+ranger --version | grep "ranger"
+pip -V
+echo "NodeJS $(node -v)"
+echo "NPM $(npm -v)"
+echo "exa $(exa -v | grep 'v')"
+bat --version
+btm --version
+
+## Config installs
 cp -r ./.config/kitty ~/.config/
 cp -r ./.config/nvim ~/.config/
 cp -r ./.config/ranger ~/.config/
 cat ./.bashrc >> ~/.bashrc
+
+neofetch
+echo "Done."
