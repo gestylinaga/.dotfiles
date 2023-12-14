6492a65bdc9862a7f240f0060dc88aa7dddaf86b
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2023-12-13 22:47:21 -0800

    updated 'pillow' install method, & added descriptions

--- Legion/devInstall.sh
+++ Legion/devInstall.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/sh
 
 echo
-echo "Installing dev tools: TypeScript, Sass, & live-server, pillow"
+echo "Installing dev tools: TypeScript, Sass, & live-server"
 echo
 
 # Base install
-sudo -H pip install pillow # ranger image preview
 sudo npm install -g typescript sass live-server # web dev
 
+echo
+echo "Installing terminal candy: pfetch, cmatrix, patched nerd font, & pillow"
+echo
+
 # Terminal candy
-paru -S --needed pfetch cmatrix-git ttf-sourcecodepro-nerd
+paru -S --needed pfetch cmatrix-git ttf-sourcecodepro-nerd python-pillow
 
 # Version check
-echo "Installed:"
+echo "Installed Versions:"
 echo
 echo "pillow $(pip show pillow | grep "Version")"
 echo "TypeScript $(tsc -v)"
